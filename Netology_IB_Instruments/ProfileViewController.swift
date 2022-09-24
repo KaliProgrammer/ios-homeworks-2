44ff1656690b1900aef6133abd78535c233e1bef
Alyona Kisha <alyonakisha1@rambler.ru>
2022-09-24 10:20:54 -0700

    First feature

---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by MacBook Air on 18.09.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
